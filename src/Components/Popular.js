224b2f8e886057d69302b1845b1261d7eec2e24e
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-07 18:02:44 +0530

    Added Popular Component

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -1,5 +1,202 @@
 import React from "react";
 
 export default function Popular() {
-  return <></>;
+  return (
+    <>
+      {/* popular section starts  */}
+      <section className="popular" id="popular">
+        <div className="heading">
+          <span>popular food</span>
+          <h3>our special dishes</h3>
+        </div>
+        <div className="box-container">
+          <div className="box">
+            <a href="#" className="fas fa-heart" />
+            <div className="image">
+              <img src={"./assests/image/food-1.png"} alt="" />
+            </div>
+            <div className="content">
+              <h3>delicious food</h3>
+              <div className="stars">
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star-half-alt" />
+                <span> (50) </span>
+              </div>
+              <div className="price">
+                $40.00 <span>$50.00</span>
+              </div>
+              <a href="#" className="btn">
+                add to cart
+              </a>
+            </div>
+          </div>
+          <div className="box">
+            <a href="#" className="fas fa-heart" />
+            <div className="image">
+              <img src={"./assests/image/food-2.png"} alt="" />
+            </div>
+            <div className="content">
+              <h3>delicious food</h3>
+              <div className="stars">
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star-half-alt" />
+                <span> (50) </span>
+              </div>
+              <div className="price">
+                $40.00 <span>$50.00</span>
+              </div>
+              <a href="#" className="btn">
+                add to cart
+              </a>
+            </div>
+          </div>
+          <div className="box">
+            <a href="#" className="fas fa-heart" />
+            <div className="image">
+              <img src={"./assests/image/food-3.png"} alt="" />
+            </div>
+            <div className="content">
+              <h3>delicious food</h3>
+              <div className="stars">
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star-half-alt" />
+                <span> (50) </span>
+              </div>
+              <div className="price">
+                $40.00 <span>$50.00</span>
+              </div>
+              <a href="#" className="btn">
+                add to cart
+              </a>
+            </div>
+          </div>
+          <div className="box">
+            <a href="#" className="fas fa-heart" />
+            <div className="image">
+              <img src={"./assests/image/food-4.png"} alt="" />
+            </div>
+            <div className="content">
+              <h3>delicious food</h3>
+              <div className="stars">
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star-half-alt" />
+                <span> (50) </span>
+              </div>
+              <div className="price">
+                $40.00 <span>$50.00</span>
+              </div>
+              <a href="#" className="btn">
+                add to cart
+              </a>
+            </div>
+          </div>
+          <div className="box">
+            <a href="#" className="fas fa-heart" />
+            <div className="image">
+              <img src={"./assests/image/food-5.png"} alt="" />
+            </div>
+            <div className="content">
+              <h3>delicious food</h3>
+              <div className="stars">
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star-half-alt" />
+                <span> (50) </span>
+              </div>
+              <div className="price">
+                $40.00 <span>$50.00</span>
+              </div>
+              <a href="#" className="btn">
+                add to cart
+              </a>
+            </div>
+          </div>
+          <div className="box">
+            <a href="#" className="fas fa-heart" />
+            <div className="image">
+              <img src={"./assests/image/food-6.png"} alt="" />
+            </div>
+            <div className="content">
+              <h3>delicious food</h3>
+              <div className="stars">
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star-half-alt" />
+                <span> (50) </span>
+              </div>
+              <div className="price">
+                $40.00 <span>$50.00</span>
+              </div>
+              <a href="#" className="btn">
+                add to cart
+              </a>
+            </div>
+          </div>
+          <div className="box">
+            <a href="#" className="fas fa-heart" />
+            <div className="image">
+              <img src={"./assests/image/food-7.png"} alt="" />
+            </div>
+            <div className="content">
+              <h3>delicious food</h3>
+              <div className="stars">
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star-half-alt" />
+                <span> (50) </span>
+              </div>
+              <div className="price">
+                $40.00 <span>$50.00</span>
+              </div>
+              <a href="#" className="btn">
+                add to cart
+              </a>
+            </div>
+          </div>
+          <div className="box">
+            <a href="#" className="fas fa-heart" />
+            <div className="image">
+              <img src={"./assests/image/food-8.png"} alt="" />
+            </div>
+            <div className="content">
+              <h3>delicious food</h3>
+              <div className="stars">
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star" />
+                <i className="fas fa-star-half-alt" />
+                <span> (50) </span>
+              </div>
+              <div className="price">
+                $40.00 <span>$50.00</span>
+              </div>
+              <a href="#" className="btn">
+                add to cart
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* popular section ends */}
+    </>
+  );
 }
